60eab8eea173c2de3384ea00d9cebeaf14ea1106
Stephen Roberts <stephenianroberts@gmail.com>
2015-08-05 17:26:57 +0000

    Makings of an scma

---
+++ scma/computearray.go
@@ -0,0 +1,42 @@
+package scma
+
+// ComputeArray represents a SCMA or other array-based computing machine
+type ComputeArray struct {
+	elements   []computeElement
+	clockLines []chan bool
+	commsLines []chan []float64
+}
+
+// Elements returns the number of ComputeElements in this array
+func (arr *ComputeArray) Elements() int {
+	return len(arr.elements)
+}
+
+func (arr *ComputeArray) Tick() {
+	for _, line := range arr.clockLines {
+		line <- true
+	}
+}
+
+// NewComputeArray builds a ComputeArray of the given dimension
+func NewComputeArray(elements int) *ComputeArray {
+	arr := new(ComputeArray)
+	arr.elements = make([]computeElement, elements)
+	arr.clockLines = make([]chan bool, elements)
+	arr.commsLines = make([]chan []float64, elements)
+
+	for i := 0; i < elements; i++ {
+		arr.clockLines[i] = make(chan bool)
+		arr.commsLines[i] = make(chan []float64)
+	}
+
+	for i := 0; i < elements; i++ {
+		element := computeElement{rank: i,
+                              data: nil,
+                              clockLine: arr.clockLines[i],
+                              dataLineEast: arr.commsLines[i],
+                              dataLineWest: arr.commsLines[(i+1)%elements]}
+    arr.elements[i] = element
+	}
+	return arr
+}
---
+++ scma/computearray_test.go
@@ -0,0 +1,27 @@
+package scma
+
+import "testing"
+
+func TestElements(t *testing.T) {
+  sizes := []int {1, 2, 5, 10, 1000}
+  for _, size := range sizes {
+    scma := NewComputeArray(size)
+    scmaSize := scma.Elements()
+    if scmaSize != size {
+      t.Error("Expected Elements() ", size, ", got ", scmaSize)
+    }
+  }
+}
+
+func TestContiguousElements(t *testing.T) {
+  sizes := []int {1, 2, 5, 10, 1000}
+  for _, size := range sizes {
+    scma := NewComputeArray(size)
+    for i := 0; i < size; i++ {
+      rank := scma.elements[i].Rank()
+      if rank != i {
+        t.Error("Unsequential Rank Detected! Expected ", i, " got ", rank)
+      }
+    }
+  }
+}
