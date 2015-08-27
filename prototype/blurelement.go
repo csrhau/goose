f79f87595ce8eec38c155a6163221b85c0dabe33
Stephen Roberts <stephenianroberts@gmail.com>
2015-08-27 16:23:39 +0000

    Work towards blur element

---
+++ prototype/blurelement.go
@@ -0,0 +1,127 @@
+package goose
+
+// BlurElement implements a ComputeElement which perfoms a gaussian blur
+type BlurElement struct {
+	rows, cols        int
+	data, scratch     [][]float64
+	northIn, northOut chan []float64
+	westIn, westOut   chan []float64
+	southIn, southOut chan []float64
+	eastIn, eastOut   chan []float64
+}
+
+// Data returns the data stored by this element
+func (el *BlurElement) Data() [][]float64 {
+	return el.data
+}
+
+// Swap causes this element to exchange data with its neighbours
+func (el *BlurElement) Swap() {
+	// Send
+	go func() {
+		westOutBuff := make([]float64, el.rows-2)
+		eastOutBuff := make([]float64, el.rows-2)
+		for r := 0; r < el.rows-2; r++ {
+			westOutBuff[r] = el.data[r+1][1]
+			eastOutBuff[r] = el.data[r+1][el.cols-2]
+		}
+		el.northOut <- el.data[1][1 : el.cols-1]
+		el.southOut <- el.data[el.rows-2][1 : el.cols-1]
+		el.westOut <- westOutBuff
+		el.eastOut <- eastOutBuff
+	}()
+
+	// Receive
+	southInBuff := <-el.southIn
+	northInBuff := <-el.northIn
+	eastInBuff := <-el.eastIn
+	westInBuff := <-el.westIn
+	for c := 0; c < el.cols-2; c++ {
+		el.data[0][c+1] = northInBuff[c]
+		el.data[el.rows-1][c+1] = southInBuff[c]
+	}
+	for r := 0; r < el.rows-2; r++ {
+		el.data[r+1][el.cols-1] = eastInBuff[r]
+		el.data[r+1][0] = westInBuff[r]
+	}
+}
+
+// Step causes this element to advance by one step
+func (el *BlurElement) Step() {
+	el.Swap()
+	// Gaussian Blur
+	kernel := [][]float64{
+		[]float64{1 / 16.0, 1 / 8.0, 1 / 16.0},
+		[]float64{1 / 8.0, 1 / 4.0, 1 / 8.0},
+		[]float64{1 / 16.0, 1 / 8.0, 1 / 16.0},
+	}
+	for i := 1; i < el.rows-1; i++ {
+		for j := 1; j < el.cols-1; j++ {
+			el.scratch[i][j] = el.data[i-1][j-1]*kernel[0][0] +
+				el.data[i-1][j]*kernel[0][1] +
+				el.data[i-1][j+1]*kernel[0][2] +
+				el.data[i][j-1]*kernel[1][0] +
+				el.data[i][j]*kernel[1][1] +
+				el.data[i][j+1]*kernel[1][2] +
+				el.data[i+1][j-1]*kernel[2][0] +
+				el.data[i+1][j]*kernel[2][1] +
+				el.data[i+1][j+1]*kernel[2][2]
+
+		}
+	}
+	el.data, el.scratch = el.scratch, el.data
+}
+
+// MakeBlurArray constructs a ComputeArray populated by BlurElements
+func MakeBlurArray(data [][]float64, widthEls, heightEls int) ComputeArray {
+	els := widthEls * heightEls
+	elRows := len(data) / heightEls
+	elCols := len(data[0]) / widthEls
+
+	// TODO TODO TODO TODO Extract the sub-array of data
+	// TODO TODO TODO TODO
+	cseelems := make([]*BlurElement, els)
+	for e := 0; e < els; e++ {
+		data := make([][]float64, elRows)
+		scratch := make([][]float64, elRows)
+		for i := 0; i < elRows; i++ {
+			data[i] = make([]float64, elCols)
+			scratch[i] = make([]float64, elCols)
+			for j := 0; j < elCols; j++ {
+				data[i][j] = float64(e)
+			}
+		}
+		cse := new(BlurElement)
+		cse.rows = elRows
+		cse.cols = elCols
+		cse.data = data
+		cse.scratch = scratch
+		cse.northOut = make(chan []float64)
+		cse.westOut = make(chan []float64)
+		cse.southOut = make(chan []float64)
+		cse.eastOut = make(chan []float64)
+		cseelems[e] = cse
+	}
+
+	// Weird naming convention, but it is correct
+	for row := 0; row < heightEls; row++ {
+		for col := 0; col < widthEls; col++ {
+			// Find neighbours, dealing with wrap-around
+			nextRow := (row + 1) % heightEls
+			prevRow := (row - 1 + heightEls) % heightEls
+			nextCol := (col + 1) % widthEls
+			prevCol := (col - 1 + widthEls) % widthEls
+			cse := cseelems[widthEls*row+col]
+			cse.northIn = cseelems[widthEls*prevRow+col].southOut
+			cse.westIn = cseelems[widthEls*row+prevCol].eastOut
+			cse.southIn = cseelems[widthEls*nextRow+col].northOut
+			cse.eastIn = cseelems[widthEls*row+nextCol].westOut
+		}
+	}
+
+	elems := make([]ComputeElement, els)
+	for i, v := range cseelems {
+		elems[i] = v
+	}
+	return ComputeArray{elements: elems}
+}
---
+++ prototype/blurelement_test.go
@@ -0,0 +1,247 @@
+package goose
+
+import "testing"
+
+func BlurElementIsAComputeElement(t *testing.T) {
+	var el ComputeElement
+	el = new(BlurElement)
+	_ = el
+}
+
+func TestBlurElementBlurs(t *testing.T) {
+	northIn := make(chan []float64)
+	northOut := make(chan []float64)
+	westIn := make(chan []float64)
+	westOut := make(chan []float64)
+	southIn := make(chan []float64)
+	southOut := make(chan []float64)
+	eastIn := make(chan []float64)
+	eastOut := make(chan []float64)
+	initData := [][]float64{
+		[]float64{0, 0, 0, 0, 0},
+		[]float64{0, 1, 1, 1, 0},
+		[]float64{0, 1, 2, 1, 0},
+		[]float64{0, 1, 1, 1, 0},
+		[]float64{0, 0, 0, 0, 0},
+	}
+	scratch := make([][]float64, 5)
+	for i := 0; i < 5; i++ {
+		scratch[i] = make([]float64, 5)
+	}
+
+	ni := []float64{0.0, 0.0, 0.0}
+	wi := []float64{0.0, 0.0, 0.0}
+	si := []float64{0.0, 0.0, 0.0}
+	ei := []float64{0.0, 0.0, 0.0}
+
+	el := new(BlurElement)
+	el.rows = 5
+	el.cols = 5
+	el.data = initData
+	el.scratch = scratch
+	el.northIn = northIn
+	el.northOut = northOut
+	el.westIn = westIn
+	el.westOut = westOut
+	el.southIn = southIn
+	el.southOut = southOut
+	el.eastIn = eastIn
+	el.eastOut = eastOut
+
+	// Drain the output buffers
+	go func() {
+		for i := 0; i < 4; i++ {
+			select {
+			case <-northOut:
+			case <-westOut:
+			case <-southOut:
+			case <-eastOut:
+			}
+		}
+	}()
+
+	// Make sure there is something waiting to be swapped
+	go func() {
+		southIn <- si
+		northIn <- ni
+		eastIn <- ei
+		westIn <- wi
+	}()
+
+	// Drain outgoing messages
+	el.Step()
+
+	expectedData := [][]float64{
+		[]float64{0, 0, 0, 0, 0},
+		[]float64{0, 0.625, 0.875, 0.625, 0},
+		[]float64{0, 0.875, 1.25, 0.875, 0},
+		[]float64{0, 0.625, 0.875, 0.625, 0},
+		[]float64{0, 0, 0, 0, 0},
+	}
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			obs := el.Data()[i][j]
+			exp := expectedData[i][j]
+			if obs != exp {
+				t.Error("Mismatch in blurred data at", i, j, "Expected:", exp, "got", obs)
+			}
+		}
+	}
+}
+
+func TestBlurElementSwapsCols(t *testing.T) {
+	northIn := make(chan []float64)
+	northOut := make(chan []float64)
+	westIn := make(chan []float64)
+	westOut := make(chan []float64)
+	southIn := make(chan []float64)
+	southOut := make(chan []float64)
+	eastIn := make(chan []float64)
+	eastOut := make(chan []float64)
+	initData := [][]float64{
+		[]float64{1.1, 1.2, 1.3, 1.4},
+		[]float64{2.1, 2.2, 2.3, 2.4},
+		[]float64{3.1, 3.2, 3.3, 3.4},
+		[]float64{4.1, 4.2, 4.3, 4.4},
+	}
+
+	ni := []float64{0.0, 0.0}
+	wi := []float64{0.1, 0.1}
+	si := []float64{0.2, 0.2}
+	ei := []float64{0.3, 0.3}
+
+	el := new(BlurElement)
+	el.rows = 4
+	el.cols = 4
+	el.data = initData
+	el.northIn = northIn
+	el.northOut = northOut
+	el.westIn = westIn
+	el.westOut = westOut
+	el.southIn = southIn
+	el.southOut = southOut
+	el.eastIn = eastIn
+	el.eastOut = eastOut
+
+	// Make sure there is something waiting to be swapped
+	go func() {
+		northIn <- ni
+		westIn <- wi
+		southIn <- si
+		eastIn <- ei
+	}()
+
+	// Run an iteration of the element logic
+	go el.Step()
+
+	// See what comes out
+	for i := 0; i < 4; i++ {
+		select {
+		case no := <-northOut:
+			if len(no) != len(ni) {
+				t.Error("Recieved", len(no), "elements from no, expected", len(ni))
+			}
+			for i, v := range no {
+				exp := initData[1][i+1]
+				if v != exp {
+					t.Error("mismatch in west received data! got", v, "expected", exp)
+				}
+			}
+		case wo := <-westOut:
+			if len(wo) != len(wi) {
+				t.Error("Recieved", len(wo), "elements from wo, expected", len(wi))
+			}
+			for i, v := range wo {
+				exp := initData[i+1][1]
+				if v != exp {
+					t.Error("mismatch in west received data! got", v, "expected", exp)
+				}
+			}
+		case so := <-southOut:
+			if len(so) != len(si) {
+				t.Error("Recieved", len(so), "elements from so, expected", len(si))
+			}
+			for i, v := range so {
+				exp := initData[2][i+1]
+				if v != exp {
+					t.Error("mismatch in west received data! got", v, "expected", exp)
+				}
+			}
+		case eo := <-eastOut:
+			if len(eo) != len(ei) {
+				t.Error("Recieved", len(eo), "elements from eo, expected", len(ei))
+			}
+			for i, v := range eo {
+				exp := initData[i+1][2]
+				if v != exp {
+					t.Error("mismatch in received data! got", v, "expected", exp)
+				}
+			}
+		}
+	}
+}
+
+func TestMakeBlurArrayPopulatesData(t *testing.T) {
+	elRows := 5
+	elCols := 7
+
+	for widthEls := 1; widthEls < 8; widthEls++ {
+		for heightEls := 1; heightEls < 8; heightEls++ {
+
+			heightCells := heightEls * elRows
+			widthCells := widthEls * elCols
+
+			blurData := make([][]float64, heightCells)
+			for i := 0; i < elRows; i++ {
+				blurData[i] = make([]float64, widthCells)
+			}
+
+			arr := MakeBlurArray(blurData, widthEls, heightEls)
+			for i, el := range arr.Elements() {
+				if len(el.Data()) != elRows || len(el.Data()[0]) != elCols {
+					t.Error("Misshapen internal data detected:", len(el.Data()), "x", len(el.Data()[0]))
+				}
+				for _, r := range el.Data() {
+					for _, c := range r {
+						if c != float64(i) {
+							t.Error("Incorrect data in initialization, expected", float64(i), "got", c, "for el", i)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestMakeBlurArrayReturnsViableArray(t *testing.T) {
+	elRows := 5
+	elCols := 7
+	heightEls := 10
+	widthEls := 10
+
+	blurData := make([][]float64, elRows*heightEls)
+	for i := 0; i < elRows; i++ {
+		blurData[i] = make([]float64, elCols*widthEls)
+	}
+
+	arr := MakeBlurArray(blurData, 10, 10)
+	arr.Step()
+}
+
+func BenchmarkBlur(b *testing.B) {
+	elRows := 25
+	elCols := 25
+	heightEls := 10
+	widthEls := 10
+
+	blurData := make([][]float64, elRows*heightEls)
+	for i := 0; i < elRows; i++ {
+		blurData[i] = make([]float64, elCols*widthEls)
+	}
+
+	arr := MakeBlurArray(blurData, widthEls, heightEls)
+	for i := 0; i < b.N; i++ {
+		arr.Step()
+	}
+}
